1a94f2f3ae6cf7327a3f6686907e47dd54315523
naelrouz <nael_rouz@mail.ru>
2016-02-16 10:11:52 +0300

    Начал 15 урок

---
+++ 15/classes.php
@@ -0,0 +1,34 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+class Publicanion {
+    public $id;
+    public $title;
+    public $date;
+    public $short_content;
+    public $content;
+    public $preview;
+    public $author_name;
+    public $type;
+ 
+    
+                function __construct() {
+        
+    }
+}
+    
+    class NewPublication extends Publicanion {
+    function __construct() {
+
+
+    }
+}
+
